cd7308dd39321b02ad8bf2f8e99d21bdee064008
galatij <stefano1.galati@mail.polimi.it>
2025-03-31 10:52:38 +0000

    Modified cpt_stress_interf.m: takes stress as input and returns normal and tangential tractions at interface only

--- atlas_matlab-main/atlas_matlab-main/bechmark_Materassi/benchmark_Galati/cpt_stress_interf.m
+++ atlas_matlab-main/atlas_matlab-main/bechmark_Materassi/benchmark_Galati/cpt_stress_interf.m
@@ -1,61 +1,76 @@
-function [stress] = cpt_stress_interf(ngauss,coord,topol,interfData, nodePairsData,E,nu,sol)
+function [sigma_n, sigma_t] = cpt_stress_interf(stress, nodePairsData)
     
-    [nodes,weights] = gausspoints(ngauss);
-    stress = zeros(nni,6);
+    nni = numel(nodePairsData);
+    sigma_n = zeros(nni,1);
+    sigma_t = zeros(nni,2);
+    for i = 1:nni
+        ntop = nodePairsData(i).ntop;
+        loc_stress = stress(ntop,:)';
+        n = nodePairsData(i).normal;
+        N = cpt_normal(n);
 
-    % Loop over the faces at interf
-    for i = 1 : ni
-        % Take the nodes of the top face and the normal
-        loc_coo = coord(interfData(i).top);
-        n = interfData(i).normal;
-        S_n = n'*cpt_normal(n);
-        
-        csi   = [-1;+1;+1;-1;-1;+1;+1;-1];
-        eta   = [-1;-1;+1;+1;-1;-1;+1;+1];
-        theta = [-1;-1;-1;-1;+1;+1;+1;+1];
-        
-        % Identify the fixed direction
-        xi = [csi,eta,theta];
-        Nloc = cpt_shape_2D(loc_coo,csi,eta,theta);
-        X = ismember(Nloc*loc_coo,coord(list,:),'row');
-    
-        for i = 1 : 3
-            if (std(xi(X,i)) == 0)
-                xi_id = i;
-                xi_val = mean(xi(X,i));
-            end
+        % Compute two orthonormal tangential basis vectors
+        if abs(n(2)) < 1e-6 && abs(n(3)) < 1e-6  % If n is aligned with z-axis
+            e1 = [0;1;0];  % Pick a different arbitrary vector
+        else
+            e1 = [1;0;0];  
         end
-    
-        ID0 = (1:3)';
-        ID = zeros(3,1);
-        ID(ID0~=xi_id) = 1:2;
+        t1 = cross(e1, n);
+        t1 = t1 / norm(t1);
+        t2 = cross(n, t1);
+        t2 = t2 / norm(t2);
 
-        D = cpt_elas_mat(E(interfData(i).etop),nu);     % 6x6 matrix
-        loc_nod = interfData(i).top;                    % 4x1 list of nodes
-        loc_coo = coord(loc_nod,:);
-        loc_dof = 3*(loc_nod-1)+v3;
-        loc_dof = loc_dof(:);
-        loc_sol = sol(loc_dof);
-        loc_eps = zeros(6,1);
-        vol = 0.0;
-
-        X3 = logical(kron(X', ones(1,3)));
-        tmp = zeros(3,1);
-        tmp(xi_id) = xi_val;
-
-        % Computation over each face
-        for i1 = 1 : ngauss
-            csi = nodes(i1);
-            tmp(ID==1) = csi;
-            for i2 = 1 : ngauss
-                eta = nodes(i2);
-                tmp(ID==2) = eta;
-                [Bloc,detJ] = cpt_shape(loc_coo,tmp(1),tmp(2),tmp(3),xi_id);        % shape derivatives 6x24
-                loc_eps = loc_eps + D*Bloc(:,X3)*loc_sol*weights(i1)*weights(i2)*detJ;
-                vol = vol + weights(i1)*weights(i2)*detJ;  
-            end
-        end
-        loc_stress = loc_eps / vol;
-        stress(i,:) = loc_stress;
+        sigma_n(i) = n'*N*loc_stress;
+        sigma_t(i,1) = t1'*N*loc_stress;
+        sigma_t(i,2) = t2'*N*loc_stress;
     end
+
 end
+
+
+% function [sigma_n, sigma_t] = cpt_stress_interf(nodePairsData,sol)
+%     
+%     nni = numel(nodePairsData);
+%     v3 = [1;2;3];
+%     stress = zeros(nni,6);
+%     sigma_n = zeros(nni,1);
+%     sigma_t = zeros(nni,2);
+%     
+%     for i = 1 : nni
+%         % Just compute the stress on the top face
+%         D = nodePairsData(i).Dtop;
+%         loc_coo = nodePairsData(i).coord;
+%         loc_nod = nodePairsData(i).ntop;
+%         loc_dof = 3*(loc_nod-1)+v3;
+%         loc_dof = loc_dof(:);
+%         loc_sol = sol(loc_dof);
+% 
+%         % Compute geometric vectors for normal and tangential
+%         % components
+%         n = nodePairsData(i).normal;
+%         N = cpt_normal(n);
+% 
+%         % TODO: generalize for general n
+%         % Compute two orthonormal tangential basis vectors
+%         if abs(n(1)) < 1e-6 && abs(n(2)) < 1e-6  % If n is aligned with z-axis
+%             e1 = [0;1;0];  % Pick a different arbitrary vector
+%         else
+%             e1 = [1;0;0];  
+%         end
+%         t1 = cross(e1, n);
+%         t1 = t1 / norm(t1);
+%         t2 = cross(n, t1);
+%         t2 = t2 / norm(t2);
+%         
+%         % Compute the stress
+%         Bloc = cpt_shape(loc_coo,0,0,0);
+%         loc_eps = Bloc*loc_sol;             % [exx; eyy; ezz; gammaxz; gammaxz; gammayz]
+%         loc_stress = D*loc_eps;             % [sxx; syy; szz; sxz; sxz; syz]
+%         stress(i,:) = loc_stress;
+%         sigma_n(i) = n'*N*loc_stress;
+%         sigma_t(i,1) = t1'*N*loc_stress;
+%         sigma_t(i,2) = t2'*N*loc_stress;
+%         % TODO: check if to do the same for the bottom face (unbiased
+%         % formulation)
+%     end
+% end
