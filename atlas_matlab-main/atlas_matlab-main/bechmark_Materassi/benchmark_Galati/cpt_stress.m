e635b10fcf3264ce07dd3e91079674792fbe874b
galatij <stefano1.galati@mail.polimi.it>
2025-03-31 10:51:33 +0000

    Modified cpt_stress.m: loop over the elements and extrapolate from gauss points, see Bathe 4.3.6

--- atlas_matlab-main/atlas_matlab-main/bechmark_Materassi/benchmark_Galati/cpt_stress.m
+++ atlas_matlab-main/atlas_matlab-main/bechmark_Materassi/benchmark_Galati/cpt_stress.m
@@ -1,21 +1,29 @@
-function [stress] = cpt_stress(ngauss,coord,ne,topol,E,nu,sol)
+function [stress] = cpt_stress(ngauss,coord,topol,E,nu,sol)
 
     [nodes,weights] = gausspoints(ngauss);
-
-    stress = zeros(ne,6);
+    ne = size(topol,1);
+    nn = size(coord,1);
+    stress = zeros(nn, 6);
+    node_count = zeros(nn,1); % counter for averaging
 
     v3 = [1;2;3];
     for i = 1 : ne
+
         % Local stiffness matrix
         D = cpt_elas_mat(E(i),nu);
 
+        % Extract topological information
         loc_nod = topol(i,:);
         loc_coo = coord(loc_nod,:);
         loc_dof = 3*(loc_nod-1)+v3;
         loc_dof = loc_dof(:);
         loc_sol = sol(loc_dof);
-        loc_eps = zeros(6,1);
-        vol = 0.0;
+
+        % Store stress at Gauss points (Bathe - FEProcedures, 4.3.6, p.254)
+        stress_gp = zeros(ngauss^3, 6);
+        gp_idx = 1;
+
+        % Evaluate stress ath each gauss point and store in stress_gp
         for i1 = 1 : ngauss
             csi = nodes(i1);
             for i2 = 1 : ngauss
@@ -23,13 +31,27 @@ function [stress] = cpt_stress(ngauss,coord,ne,topol,E,nu,sol)
                 for i3 = 1 : ngauss
                     theta = nodes(i3);
                     [Bloc,detJ] = cpt_shape(loc_coo,csi,eta,theta);
-                    loc_eps = loc_eps + Bloc*loc_sol*weights(i1)*weights(i2)*weights(i3)*detJ;
-                    vol = vol + weights(i1)*weights(i2)*weights(i3)*detJ;
+                    % TODO: check if it's better to write the DOFs
+                    % separately (loc_eps --> 6x3)
+                    loc_eps = Bloc*loc_sol; % 6x24 * 24x1 --> 6x1 (or 6x3)
+
+                    stress_gp(gp_idx,:) = (D*loc_eps)'; % 6x1 (or 6x3)
+                    gp_idx = gp_idx + 1;
                 end
             end
         end
-        loc_stress = loc_eps / vol;
-        stress(i,:) = loc_stress;
+        % Loop over the local nodes and add contribution to global nodes
+        for n = 1 : 8
+            nod = loc_nod(n);
+            % TODO: take only the closest gauss points (?)
+            stress(nod,:) = stress(nod,:) + mean(stress_gp,1);
+            node_count(nod) = node_count(nod) + 1;
+        end
     end
 
+    for i = 1:nn
+        if node_count(i) > 0
+            stress(i, :) = stress(i, :) / node_count(i);
+        end
+    end
 end
